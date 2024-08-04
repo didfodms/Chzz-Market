52e61c33d21fb9dbabf2fc3c43f79b30159b4e07
reyang <didfodms@gmail.com>
2024-08-05 01:31:46 +0900

    fix: Naver 닉네임 결측값 오류 수정

--- src/main/java/org/chzz/market/domain/oauth/dto/NaverUserInfoResponse.java
+++ src/main/java/org/chzz/market/domain/oauth/dto/NaverUserInfoResponse.java
@@ -26,7 +26,7 @@ public class NaverUserInfoResponse {
     }
 
     public String getNickname() {
-        return String.valueOf(attributes.get("name"));
+        return String.valueOf(attributes.get("nickname"));
     }
 
 }
