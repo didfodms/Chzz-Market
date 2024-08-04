079317bc503eb0bb807e0846824b0d06279bd8d0
reyang <didfodms@gmail.com>
2024-08-05 00:49:09 +0900

    feat: Kakao API 연결 기능 구현

--- src/main/java/org/chzz/market/domain/oauth/controller/KakaoOAuth2Controller.java
+++ src/main/java/org/chzz/market/domain/oauth/controller/KakaoOAuth2Controller.java
@@ -1,12 +1,34 @@
 package org.chzz.market.domain.oauth.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/login/kakao")
 public class KakaoOAuth2Controller {
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    String clientId;
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    String redirectUri;
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    String clientSecret;
+
+    // 카카오 API 연결
+    @GetMapping(value="")
+    public String connect() {
+        // 카카오톡에서 자동 로그인
+        StringBuilder url = new StringBuilder();
+        url.append("https://kauth.kakao.com/oauth/authorize?");
+        url.append("client_id=" + clientId);
+        url.append("&redirect_uri=" + redirectUri);
+        url.append("&response_type=code");
+
+        return "redirect:" + url.toString();
+    }
 
 }
