32ce21d2a46b8554614215d355a0daef2373aecb
reyang <didfodms@gmail.com>
2024-08-05 01:16:45 +0900

    refactor: 회원 여부 확인 및 회원 조회를 위한 메소드 추가

--- src/main/java/org/chzz/market/domain/user/repository/UserRepository.java
+++ src/main/java/org/chzz/market/domain/user/repository/UserRepository.java
@@ -4,4 +4,6 @@ import org.chzz.market.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByEmail(String email);
+    User findByEmail(String email);
 }
