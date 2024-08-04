0bc7453118b798bdba238034c01dd88b6421e825
reyang <didfodms@gmail.com>
2024-08-05 00:44:10 +0900

    create: Kakao 로그인/회원가입 컨트롤러 생성

---
+++ src/main/java/org/chzz/market/domain/oauth/controller/KakaoOAuth2Controller.java
@@ -0,0 +1,12 @@
+package org.chzz.market.domain.oauth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/login/kakao")
+public class KakaoOAuth2Controller {
+
+}
