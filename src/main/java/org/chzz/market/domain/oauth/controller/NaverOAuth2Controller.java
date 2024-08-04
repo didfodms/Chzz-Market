db08ac175b6ec4c929cf245aec433975756e1d3e
reyang <didfodms@gmail.com>
2024-08-05 01:17:58 +0900

    create: Naver 로그인/회원가입 컨트롤러 클래스 생성

---
+++ src/main/java/org/chzz/market/domain/oauth/controller/NaverOAuth2Controller.java
@@ -0,0 +1,13 @@
+package org.chzz.market.domain.oauth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/login/naver")
+public class NaverOAuth2Controller {
+
+}
+
