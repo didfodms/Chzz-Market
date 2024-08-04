bce320caf00b8847a4ccaa78fbb5722f5473c296
reyang <didfodms@gmail.com>
2024-08-05 01:23:53 +0900

    create: Naver 유저 정보 Dto 클래스 생성

---
+++ src/main/java/org/chzz/market/domain/oauth/dto/NaverUserInfoResponse.java
@@ -0,0 +1,32 @@
+package org.chzz.market.domain.oauth.dto;
+
+import lombok.NoArgsConstructor;
+import org.chzz.market.domain.user.entity.User.ProviderType;
+
+import java.util.Map;
+
+@NoArgsConstructor
+public class NaverUserInfoResponse {
+    private Map<String, Object> attributes;
+
+    public NaverUserInfoResponse(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getProviderId() {
+        return String.valueOf(attributes.get("id"));
+    }
+
+    public ProviderType getProviderType() {
+        return ProviderType.NAVER;
+    }
+
+    public String getEmail() {
+        return String.valueOf(attributes.get("email"));
+    }
+
+    public String getNickname() {
+        return String.valueOf(attributes.get("name"));
+    }
+
+}
