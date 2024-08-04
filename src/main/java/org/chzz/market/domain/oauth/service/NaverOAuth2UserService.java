ec3b6beae9bb2266fe9cef8853ced350282b6f98
reyang <didfodms@gmail.com>
2024-08-05 01:21:47 +0900

    feat: Naver 액세스 토큰 발급 기능 구현

--- src/main/java/org/chzz/market/domain/oauth/service/NaverOAuth2UserService.java
+++ src/main/java/org/chzz/market/domain/oauth/service/NaverOAuth2UserService.java
@@ -1,8 +1,62 @@
 package org.chzz.market.domain.oauth.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class NaverOAuth2UserService{
+    @Value("${spring.security.oauth2.client.registration.naver.client-id}")
+    String clientId;
+    @Value("${spring.security.oauth2.client.registration.naver.redirect-uri}")
+    String redirectUri;
+    @Value("${spring.security.oauth2.client.registration.naver.client-secret}")
+    String clientSecret;
+    @Value("${spring.security.oauth2.client.registration.naver.authorization-grant-type}")
+    String authorizationGrantType;
 
+    // NAVER 액세스 토큰 발급
+    public String getAccessToken(String code) throws JsonProcessingException {
+
+        // 1. header 생성
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8");
+
+        // 2. body 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", authorizationGrantType); //고정값
+        params.add("client_id", clientId);
+        params.add("redirect_uri", redirectUri);
+        params.add("client_secret", clientSecret);
+        params.add("code", code);
+
+        // 3. header + body
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(params, httpHeaders);
+
+        // 4. http 요청
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(
+                "https://nid.naver.com/oauth2.0/token",
+                HttpMethod.POST,
+                httpEntity,
+                String.class
+        );
+
+        // 5. HTTP 응답 (JSON) 액세스 토큰 파싱
+        String responseBody = response.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+
+        // 6. 카카오 액세스 토큰 반환
+        return jsonNode.get("access_token").asText();
+    }
 }
